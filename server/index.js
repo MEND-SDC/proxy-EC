972377dbc9ffcfa81af20c6fcdb167da6038521a
elchangtw <elchang.tw@gmail.com>
2020-01-09 15:20:34 -0800

    preparing for deployment

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const proxy = require('http-proxy-middleware');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,23 +16,27 @@ app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
 
+app.use('/loaderio-8cbfd155759247c75ceb1cee030d0375.txt', express.static(path.join(__dirname, '../public/loaderio-8cbfd155759247c75ceb1cee030d0375.txt')));
 
 
-const suggestions = {
-  target: 'http://localhost:3001',
+
+const photogalleries = {
+  // target: 'http://localhost:3004',
+  target: 'http://35.162.183.181',
   changeOrigin: true,
   onProxyRes: function (proxyRes) {
     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
   }
 };
-const suggestion = proxy(suggestions);
-app.use('/', suggestion);
+const photogallery = proxy(photogalleries);
+app.use('/', photogallery);
 
 
 
 const reservations = {
   // target: 'http://3.135.103.1',
-  target: 'http://localhost:3002',
+  // target: 'http://localhost:3002',
+  target: 'http://13.52.45.163',
   changeOrigin: true,
   onProxyRes: function (proxyRes) {
     proxyRes.headers['Access-Control-Allow-Origin'] = '*';
@@ -40,6 +45,19 @@ const reservations = {
 
 const reservation = proxy(reservations);
 app.use('/', reservation);
+
+
+
+const suggestions = {
+  // target: 'http://localhost:3001',
+  target: '52.53.156.46',
+  changeOrigin: true,
+  onProxyRes: function (proxyRes) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+};
+const suggestion = proxy(suggestions);
+app.use('/', suggestion);
 
 
 
@@ -53,16 +71,3 @@ const reviews = {
 
 const review = proxy(reviews);
 app.use('/', review);
-
-
-
-const photogalleries = {
-  target: 'http://localhost:3004',
-  changeOrigin: true,
-  onProxyRes: function (proxyRes) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  }
-};
-const photogallery = proxy(photogalleries);
-app.use('/', photogallery);
-
